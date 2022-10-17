33383adfe2291d5ca90c572a226c45b3c3ff3d9b
Иван Сиснёв <ivan.sisnev@gmail.com>
2022-10-17 14:04:04 +0300

    01

--- homework.py
+++ homework.py
@@ -1,82 +1,192 @@
-...
+"""
+Модуль проверки статуса домашних заданий, находящихся на проверке.
+Связывается с API сервиса, проверяет полученные данные. При наличии
+обновленных статусов домашних заданий, отправляет сообщение о них в чат
+Телеграм.
+"""
+import logging
+import sys
+import time
 
-load_dotenv()
+import requests
+import telegram
 
-
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
-
-RETRY_TIME = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-
-
-HOMEWORK_STATUSES = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.'
-}
+from exceptions import *
+from settings import *
 
 
-def send_message(bot, message):
-    ...
+# Создание логера
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(handler)
+handler = logging.FileHandler('log.txt', encoding=ENCODING)
+logger.addHandler(handler)
+formatter = logging.Formatter('%(asctime)s уровень %(levelname)s, функция %('
+                              'funcName)s: %(message)s')
+handler.setFormatter(formatter)
 
 
-def get_api_answer(current_timestamp):
-    timestamp = current_timestamp or int(time.time())
-    params = {'from_date': timestamp}
+def send_message(bot, message: str) -> None:
+    """
+    Отправляет сообщение в чат.
+    :param bot: экземпляр класса telegram.Bot
+    :param message: текст сообщения
+    """
+    if bot.send_message(TELEGRAM_CHAT_ID, message):
+        logger.info('Сообщение в чат успешно отправлено.')
+    else:
+        logger.error(BotMalfunction.message)
+    return None
 
-    ...
+def get_api_answer(timestamp: int) -> dict:
+    """
+    Делает запрос к эндпоинту API-сервиса и возвращает полученные данные в
+    виде словаря.
+    :param timestamp: метка времени
+    :return response: словарь данными
+    """
+    params: dict = {'from_date': timestamp}
+
+    response = requests.get(ENDPOINT, headers=HEADERS, params=params,
+                            timeout=10)
+
+    if response.status_code != 200:
+        logger.error(APIAccessError.message)
+        raise APIAccessError
+
+    response = dict(response.json())
+
+    if not isinstance(response, dict):
+        logger.error(APIResponseError.message)
+        raise APIResponseError
+
+    logger.info('Ответ от API получен. '
+                'Словарь с данными передан дальше.')
+    return response
 
 
-def check_response(response):
+def check_response(response: dict) -> list:
+    """
+    Проверяет данные, полученные от API, на корректность и возвращает список
+    домашних работ.
+    :param response: словарь с данными
+    :return: hw_list: список домашних работ
+    """
+    if 'homeworks' not in response:
+        logger.error(DataError.message)
+        raise KeyError
 
-    ...
+    hw_list = response.get('homeworks')
+
+    if not isinstance(hw_list, list):
+        logger.error(DataError.message)
+        raise TypeError
+
+    logger.info('Словарь с данными проверен. Список домашних заданий '
+                'передан дальше.')
+    return hw_list
 
 
-def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
+def parse_status(homework: dict) -> str:
+    """
+    Получает из словаря с данными домашнего задания его статус и возвращает
+    строку c названием задания и вердиктом, соответствующим статусу в словаре
+    HOMEWORK_STATUSES.
+    :param homework: словарь с данными домашнего задания
+    :return verdict: строка с вердиктом
+    """
+    if 'homework_name' not in homework or 'status' not in homework:
+        logger.error(DataError.message)
+        raise KeyError
 
-    ...
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
 
-    verdict = ...
+    if homework_status not in HOMEWORK_STATUSES:
+        logger.error(DataError.message)
+        raise KeyError
 
-    ...
+    verdict = HOMEWORK_STATUSES.get(homework_status)
+    logger.info('Обновленный статус домашней работы получен. Вердикт '
+                'передан дальше.')
+    return (f'Изменился статус проверки работы "{homework_name}". '
+            f'{verdict}')
 
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
-
-def check_tokens():
-    ...
+def check_tokens() -> bool:
+    """
+    Проверяет доступность констант из settings.py.
+    :return: bool
+    """
+    if not all(
+            (
+                    PRACTICUM_TOKEN,
+                    TELEGRAM_TOKEN,
+                    TELEGRAM_CHAT_ID,
+                    ENDPOINT,
+                    HEADERS,
+            )
+    ) or not isinstance(HOMEWORK_STATUSES, dict) \
+            or not all(key in HOMEWORK_STATUSES for key in (
+            ('approved', 'reviewing', 'rejected'))
+                       ):
+        return False
+    logger.debug('Все токены и константы в порядке.')
+    return True
 
 
 def main():
-    """Основная логика работы бота."""
+    """
+    Основная функция с главной логикой бота.
+    Проверяет необходимые переменные, делает запрос к API, проверяет ответ и
+    полученные данные; в случае наличия обновлений получает строку с
+    вердиктом и отправляет ее в чат.
+    """
 
-    ...
+    # бот Телеграм
+    bot = telegram.Bot(TELEGRAM_TOKEN)
 
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
+    # Проверка токенов и констант
+    if not check_tokens():
+        logger.critical(TokenError.message)
+        send_message(bot, TokenError.message)
+        raise TokenError(TokenError.message)
 
-    ...
+    # Метка времени для запроса к API
+    timestamp = int(time.time())
+    # Переменная однократного сообщения об ошибке запроса к API
+    notified = False
 
     while True:
         try:
-            response = ...
+            # Получение данных от API
+            response = get_api_answer(timestamp)
 
-            ...
+            # Метка времени для последующих запросов
+            timestamp = response.get('current_date') or timestamp
 
-            current_timestamp = ...
-            time.sleep(RETRY_TIME)
+            # Выделение списка домашних заданий из полученных от API данных
+            hw_list = check_response(response)
 
-        except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            ...
-            time.sleep(RETRY_TIME)
-        else:
-            ...
+            # Проверка списка домашних заданий
+            if hw_list:
+                # Отправка обновленных статусов в чат
+                for homework in hw_list:
+                    verdict = parse_status(homework)
+                    logger.info('Вердикт передан на отправку в чат.')
+                    send_message(bot, verdict)
+            else:
+                logger.debug('Новых статусов домашних заданий нет.')
+
+        except APIAccessError:
+            logger.warning(APIAccessError.message)
+            if not notified:
+                send_message(bot, APIAccessError.message)
+                notified = True
+
+        finally:
+            time.sleep(RETRY_TIME or 600)
 
 
 if __name__ == '__main__':
---
+++ settings.py
@@ -0,0 +1,39 @@
+"""
+Файл с константами и настройками для homework.py.
+"""
+
+import os
+from typing import Final
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Персональный токен пользователя Яндекс.Практикум
+PRACTICUM_TOKEN: Final = os.getenv('PRACTICUM_TOKEN')
+
+# Токен бота Телеграм
+TELEGRAM_TOKEN: Final = os.getenv('TELEGRAM_TOKEN')
+
+# ID чата для отправки сообщений
+TELEGRAM_CHAT_ID: Final = os.getenv('TELEGRAM_CHAT_ID')
+
+# URL запроса (эндпоинт API)
+ENDPOINT: Final = ('https://practicum.yandex.ru/api/user_api'
+                   '/homework_statuses/')
+
+# Авторизационная информация для запроса к API
+HEADERS: Final = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+
+# Время в секундах между запросами к API
+RETRY_TIME: Final = 600
+
+# Словарь со статусами проверки работ
+HOMEWORK_STATUSES: dict = {
+    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
+    'reviewing': 'Работа взята на проверку ревьюером.',
+    'rejected': 'Работа проверена: у ревьюера есть замечания.'
+}
+
+# Кодировка для лог-файла
+ENCODING: str = 'UTF-8'
