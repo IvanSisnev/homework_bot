bc72d27ee6ef0dd8c9f0f2f0355a5bd4ad826ad9
Иван Сиснёв <ivan.sisnev@gmail.com>
2022-10-19 14:48:21 +0300

    09

--- settings.py
+++ settings.py
@@ -3,30 +3,28 @@
 """
 
 import os
-from typing import Final
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
 # Персональный токен пользователя Яндекс.Практикум
-PRACTICUM_TOKEN: Final = os.getenv('PRACTICUM_TOKEN')
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 
 # Токен бота Телеграм
-TELEGRAM_TOKEN: Final = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
 # ID чата для отправки сообщений
-TELEGRAM_CHAT_ID: Final = os.getenv('TELEGRAM_CHAT_ID')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 # URL запроса (эндпоинт API)
-ENDPOINT: Final = ('https://practicum.yandex.ru/api/user_api'
-                   '/homework_statuses/')
+ENDPOINT: str = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 
 # Авторизационная информация для запроса к API
-HEADERS: Final = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+HEADERS: dict = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 # Время в секундах между запросами к API
-RETRY_TIME: Final = 600
+RETRY_TIME: int = 600
 
 # Словарь со статусами проверки работ
 HOMEWORK_STATUSES: dict = {
